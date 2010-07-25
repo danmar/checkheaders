0096ed14ecadcfb2c3afae94cda6e91196abcf8f
daniel.marjamaki <daniel.marjamaki@ee43edf2-f3fe-2ba9-66c8-bd1c76ca33d9>
2010-07-25 17:31:27 +0000

    some #define support removed. added testcase TestWarningIncludeHeaders::needed_define

--- test/testwarningincludeheaders.cpp
+++ test/testwarningincludeheaders.cpp
@@ -37,54 +37,26 @@ private:
     void run()
     {
         TEST_CASE(issue3);
+        TEST_CASE(needed_define);
         TEST_CASE(test1);
-    }
-
-    void test1()
-    {
-        {
-            std::ofstream f1("a.c");
-            f1 << "#include \"a.h\"\n"
-               << "int i[NUM];\n";
-
-            std::ofstream f2("a.h");
-            f2 << "#include \"fred.h\"\n"
-               << "const int NUM = 10;\n";
-
-            std::ofstream f3("fred.h");
-            f3 << "class Fred { };\n";
-        }
-
-        tokens = tokens_back = NULL;
-        Files.clear();
-        Tokenize("a.c");
-
-        // Including header which is not needed
-        std::ostringstream errout;
-        WarningIncludeHeader(errout);
-
-        // Clean up tokens..
-        DeallocateTokens();
-
-        ASSERT_EQUALS("[a.h:1] (style): The included header 'fred.h' is not needed\n", errout.str());
     }
 
     void issue3()
     {
         {
-            std::ofstream f1("a.c");
-            f1 << "#include \"fred.h\"\n"
+            std::ofstream f1("issue3.c");
+            f1 << "#include \"issue3.h\"\n"
                << "struct APP_INIT_DATA {\n"
                << "  PROXY_INFO proxy_info;\n"
                << "};\n";
 
-            std::ofstream f2("a.h");
+            std::ofstream f2("issue3.h");
             f2 << "struct PROXY_INFO { bool use_http_proxy; };\n";
         }
 
         tokens = tokens_back = NULL;
         Files.clear();
-        Tokenize("a.c");
+        Tokenize("issue3.c");
 
         // Including header which is not needed
         std::ostringstream errout;
@@ -94,6 +66,55 @@ private:
         DeallocateTokens();
 
         ASSERT_EQUALS("", errout.str());
+    }
+
+    void needed_define()
+    {
+        {
+            std::ofstream f1("needed_define.c");
+            f1 << "#include \"needed_define.h\"\n"
+               << "void foo() __attribute__((deprecated));\n";
+
+            std::ofstream f2("needed_define.h");
+            f2 << "#define __attribute__(x)\n";
+        }
+
+        tokens = tokens_back = NULL;
+        Files.clear();
+        Tokenize("needed_define.c");
+
+        // Including header which is not needed
+        std::ostringstream errout;
+        WarningIncludeHeader(errout);
+
+        // Clean up tokens..
+        DeallocateTokens();
+
+        ASSERT_EQUALS("", errout.str());
+    }
+    
+    void test1()
+    {
+        {
+            std::ofstream f1("test1.c");
+            f1 << "#include \"test1.h\"\n";
+
+            std::ofstream f2("test1.h");
+            f2 << "class Fred { };\n";
+        }
+
+        tokens = tokens_back = NULL;
+        Files.clear();
+        Tokenize("test1.c");
+
+        // Including header which is not needed
+        std::ostringstream errout;
+        WarningIncludeHeader(errout);
+
+        // Clean up tokens..
+        DeallocateTokens();
+
+        ASSERT_EQUALS("[test1.c:1] (style): The included header 'test1.h' is not needed\n", errout.str());
     }
 };
 
